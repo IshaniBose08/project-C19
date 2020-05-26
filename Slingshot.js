76788fc252db4d093979a8cac361e50bf197cdcc
IshaniBose08 <63492168+IshaniBose08@users.noreply.github.com>
2020-05-26 03:56:02 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,43 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
+
+        World.add(world, this.sling);
+        
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(body){
+     this.sling.bodyA = body;
+    }
+
+    display(){
+        image(this.image1 , 200,25); 
+        image(this.image2,170,36);
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            fill('#301608')
+            line(pointA.x - 20, pointA.y, pointB.x- 30, pointB.y)
+            
+            line(pointA.x   + 20, pointA.y, pointB.x +20 , pointB.y);
+            image(this.image3 , pointA.x-25,pointA.y -10 ,6 ,30)
+        }
+    }
+    
+}
